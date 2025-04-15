813e5d8ccfaf74d73ad0feb3ce1ff90c778f4164
asifur.rahman <asifur.rahman@spectrum-bd.com>
2025-04-15 13:50:05 +0600

    updated readme

--- dnn_face_recog/insight_face.py
+++ dnn_face_recog/insight_face.py
@@ -19,10 +19,10 @@ app.prepare(ctx_id=-1)  # Use -1 for CPU, or specify a GPU ID if available
 # model.prepare(ctx_id=-1) face_recg2\face_recog_deepNN\dnn_face_recog\videos\harry_potter_premier.mp4
 # face_recg2\face_recog_deepNN\dnn_face_recog\videos\received_1179123699699768.mp4 face_recg2\face_recog_deepNN\dnn_face_recog\videos\face-demographics-walking.mp4
 video_path = 'videos/ron.mp4'  # Replace with your video file pathface_recg2\face_recog_deepNN\dnn_face_recog\videos\classroom.mp4
-# video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 rtsp_url="rtsp://admin:Sscl1234@192.168.1.108:554/cam/realmonitor?channel=1&subtype=0"
 # video_capture = cv2.VideoCapture(video_path)
-video_capture = cv2.VideoCapture(rtsp_url)
+# video_capture = cv2.VideoCapture(rtsp_url)
 
 output_video_path = 'videos/output_video_with_faces.mp4'
 
